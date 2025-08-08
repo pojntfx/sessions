c3862970f214eed1432cac3b3870cf35567987fe
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-08-07 23:02:52 -0700

    fix: Build locales without need for GNU parallel
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- config/locales/index.go
+++ config/locales/index.go
@@ -2,6 +2,6 @@ package locales
 
 import "embed"
 
-//go:generate sh -c "find . -type f -name *.po | parallel --progress msgfmt -o {.}.mo {}"
+//go:generate sh -c "find . -type f -name '*.po' -print0 | xargs -0 -I {} sh -c 'msgfmt -o \"{}.mo\" \"{}\"' && find . -type f -name '*.po.mo' -exec sh -c 'mv \"{}\" \"$(echo \"{}\" | sed s/\\.po\\.mo/.mo/)\"' \\;"
 //go:embed *
 var FS embed.FS
