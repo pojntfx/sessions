f57ee5b22946048c1b2a7fa0116f0f79f28a1804
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-08-09 14:40:26 -0700

    refactor: Use `xgettext` properly for locales
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- config/locales/index.go
+++ config/locales/index.go
@@ -2,6 +2,8 @@ package locales
 
 import "embed"
 
+//go:generate sh -c "find ../.. -name '*.go' -o -name '*.blp' | xgettext --language=C++ --keyword=_ --keyword=gcore.Local --keyword=Local -o default.pot --files-from=-"
+//go:generate sh -c "find . -name 'default.po' -print0 | xargs -0 -I {} msgmerge --update \"{}\" default.pot"
 //go:generate sh -c "find . -type f -name '*.po' -print0 | xargs -0 -I {} sh -c 'msgfmt -o \"{}.mo\" \"{}\"' && find . -type f -name '*.po.mo' -exec sh -c 'mv \"{}\" \"$(echo \"{}\" | sed s/\\.po\\.mo/.mo/)\"' \\;"
 //go:embed *
 var FS embed.FS
