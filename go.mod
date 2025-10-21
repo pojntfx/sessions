a0c3e5a88158c128da4952901cc353a66dfeeacf
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-10-20 23:50:07 -0700

    feat: Start source port from gotk4 to puregotk4
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- go.mod
+++ go.mod
@@ -4,16 +4,13 @@ go 1.24.4
 
 tool github.com/dennwc/flatpak-go-mod
 
-require (
-	github.com/diamondburned/gotk4-adwaita/pkg v0.0.0-20250703085337-e94555b846b6
-	github.com/diamondburned/gotk4/pkg v0.3.2-0.20250703063411-16654385f59a
-)
+require github.com/jwijenbergh/puregotk v0.0.0-20251017112506-edf23b7ceb5e
 
 require (
-	github.com/KarpelesLab/weak v0.1.1 // indirect
 	github.com/dennwc/flatpak-go-mod v0.1.1-0.20250809093520-ddf8d84264aa // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
-	go4.org/unsafe/assume-no-moving-gc v0.0.0-20231121144256-b99613f794b6 // indirect
+	github.com/jwijenbergh/purego v0.0.0-20251017112123-b71757b9ba42 // indirect
 	golang.org/x/mod v0.27.0 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 )
+
+replace github.com/jwijenbergh/puregotk v0.0.0-20251017112506-edf23b7ceb5e => ../puregotk // github.com/pojntfx/puregotk v0.0.0-20251018041106-062cbe4143b9
--- main.go
+++ main.go
@@ -1,80 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/fs"
-	"math"
 	"os"
-	"path/filepath"
-	"time"
 
-	"github.com/diamondburned/gotk4-adwaita/pkg/adw"
-	"github.com/diamondburned/gotk4/pkg/cairo"
-	gcore "github.com/diamondburned/gotk4/pkg/core/glib"
-	"github.com/diamondburned/gotk4/pkg/gdk/v4"
-	"github.com/diamondburned/gotk4/pkg/gio/v2"
-	"github.com/diamondburned/gotk4/pkg/glib/v2"
-	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+	"github.com/jwijenbergh/puregotk/v4/adw"
+	"github.com/jwijenbergh/puregotk/v4/gio"
+	"github.com/jwijenbergh/puregotk/v4/glib"
+	"github.com/jwijenbergh/puregotk/v4/gtk"
 	"github.com/pojntfx/sessions/assets/resources"
-	po "github.com/pojntfx/sessions/po"
 )
 
 //go:generate sh -c "if [ -z \"$FLATPAK_ID\" ]; then go tool github.com/dennwc/flatpak-go-mod --json .; fi"
 
 func main() {
-	i18t, err := os.MkdirTemp("", "")
+	resource, err := gio.NewResourceFromData(glib.NewBytes(resources.ResourceContents, uint(len(resources.ResourceContents))))
 	if err != nil {
 		panic(err)
 	}
-	defer os.RemoveAll(i18t)
+	gio.ResourcesRegister(resource)
 
-	if err := fs.WalkDir(po.FS, ".", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if d.IsDir() {
-			if err := os.MkdirAll(filepath.Join(i18t, path), os.ModePerm); err != nil {
-				return err
-			}
-
-			return nil
-		}
-
-		src, err := po.FS.Open(path)
-		if err != nil {
-			return err
-		}
-		defer src.Close()
-
-		dst, err := os.Create(filepath.Join(i18t, path))
-		if err != nil {
-			return err
-		}
-		defer dst.Close()
-
-		if _, err := io.Copy(dst, src); err != nil {
-			return err
-		}
-
-		return nil
-	}); err != nil {
-		panic(err)
-	}
-
-	gcore.InitI18n("default", i18t)
-
-	r, err := gio.NewResourceFromData(glib.NewBytesWithGo(resources.ResourceContents))
-	if err != nil {
-		panic(err)
-	}
-	gio.ResourcesRegister(r)
-
-	a := adw.NewApplication(resources.AppID, gio.ApplicationDefaultFlags)
+	a := adw.NewApplication(resources.AppID, gio.GApplicationDefaultFlagsValue)
 
 	var w *adw.ApplicationWindow
-	a.ConnectActivate(func() {
+	cb := func(gio.Application) {
 		if w != nil {
 			w.Present()
 
@@ -88,321 +36,23 @@ func main() {
 
 		b := gtk.NewBuilderFromResource(resources.ResourceWindowUIPath)
 
-		w = b.GetObject("main_window").Cast().(*adw.ApplicationWindow)
-		var (
-			dial   = b.GetObject("dial_area").Cast().(*gtk.DrawingArea)
-			label  = b.GetObject("analog_time_label").Cast().(*gtk.Label)
-			action = b.GetObject("action_button").Cast().(*gtk.Button)
-			plus   = b.GetObject("plus_button").Cast().(*gtk.Button)
-			minus  = b.GetObject("minus_button").Cast().(*gtk.Button)
-		)
-
-		var (
-			alarmClockElapsedFile = gtk.NewMediaFileForResource(resources.ResourceAlarmClockElapsedPath)
-		)
-		startAlarmPlayback := func() {
-			alarmClockElapsedFile.Seek(0)
-			alarmClockElapsedFile.Play()
-		}
-
-		stopAlarmPlayback := func() {
-			alarmClockElapsedFile.SetPlaying(false)
-			alarmClockElapsedFile.Seek(0)
-		}
-
-		var (
-			totalSec = 300
-			running  = false
-		)
-		updateButtons := func() {
-			if running {
-				action.SetIconName("media-playback-stop-symbolic")
-				action.SetLabel(gcore.Local("Stop"))
-				action.RemoveCSSClass("suggested-action")
-				action.AddCSSClass("destructive-action")
-			} else {
-				action.SetIconName("media-playback-start-symbolic")
-				action.SetLabel(gcore.Local("Start Timer"))
-				action.RemoveCSSClass("destructive-action")
-				action.AddCSSClass("suggested-action")
-			}
-
-			plus.SetSensitive(totalSec < 3600)
-			minus.SetSensitive(totalSec > 30)
-		}
-
-		var (
-			remain = time.Duration(0)
-		)
-		updateDial := func() {
-			var m, s int
-			if running {
-				m, s = int(remain.Minutes()), int(remain.Seconds())%60
-			} else {
-				m, s = totalSec/60, totalSec%60
-			}
-
-			label.SetText(fmt.Sprintf("%02d:%02d", m, s))
-			dial.QueueDraw()
-		}
-
-		var (
-			timer = glib.SourceHandle(0)
-		)
-		createSessionFinishedHandler := func() func() bool {
-			return func() bool {
-				if !running {
-					return false
-				}
-
-				remain -= time.Second
-				updateDial()
-
-				if remain <= 0 {
-					running = false
-					if timer > 0 {
-						glib.SourceRemove(timer)
-						timer = 0
-					}
-
-					updateButtons()
-					updateDial()
-
-					startAlarmPlayback()
-
-					n := gio.NewNotification(gcore.Local("Session finished"))
-					n.SetPriority(gio.NotificationPriorityUrgent)
-					n.SetDefaultAction("app.stopAlarmPlayback")
-					n.AddButton(gcore.Local("Stop alarm"), "app.stopAlarmPlayback")
-
-					a.SendNotification("session-finished", n)
-
-					return false
-				}
-
-				return true
-			}
-		}
-
-		startTimer := func() {
-			stopAlarmPlayback()
-
-			running = true
-			remain = time.Duration(totalSec) * time.Second
-
-			updateButtons()
-			updateDial()
-
-			timer = glib.TimeoutAdd(1000, createSessionFinishedHandler())
-		}
-
-		stopTimer := func() {
-			running = false
-			if timer > 0 {
-				glib.SourceRemove(timer)
-				timer = 0
-			}
-
-			updateButtons()
-			updateDial()
-		}
-
-		resumeTimer := func() {
-			if remain > 0 {
-				timer = glib.TimeoutAdd(1000, createSessionFinishedHandler())
-			}
-		}
-
-		var (
-			dragging = false
-			paused   = false
-		)
-		handleDialing := func(x, y float64) {
-			if running && !dragging {
-				paused = true
-				if timer > 0 {
-					glib.SourceRemove(timer)
-					timer = 0
-				}
-			}
-
-			w, h := float64(dial.Width()), float64(dial.Height())
-			cx, cy := w/2, h/2
-			dx, dy := x-cx, y-cy
-
-			if math.Sqrt(dx*dx+dy*dy) < 15 {
-				return
-			}
-
-			a := math.Atan2(dy, dx) + math.Pi/2
-			if a < 0 {
-				a += 2 * math.Pi
-			}
-
-			intervals := int((a / (2 * math.Pi)) * 120)
-			if intervals == 0 {
-				intervals = 120
-			}
-
-			totalSec = intervals * 30
-
-			if paused {
-				remain = time.Duration(totalSec) * time.Second
-			}
-
-			updateDial()
-		}
-
-		dial.SetDrawFunc(func(area *gtk.DrawingArea, cr *cairo.Context, w, h int) {
-			cx, cy := float64(w)/2, float64(h)/2
-			r := math.Min(cx, cy) - 15
-
-			style := area.StyleContext()
-			accent, _ := style.LookupColor("accent_bg_color")
-			err, _ := style.LookupColor("error_bg_color")
-			cr.SetSourceRGB(0.7, 0.7, 0.7)
-			cr.SetLineWidth(10)
-			cr.Arc(cx, cy, r, 0, 2*math.Pi)
-			cr.Stroke()
-
-			if totalSec > 0 {
-				progress := float64(totalSec) / 3600.0
-				end := -math.Pi/2 + 2*math.Pi*progress
-				var (
-					handleAngle                float64
-					handleColor                *gdk.RGBA
-					angle                      float64
-					lineColor                  *gdk.RGBA
-					fillR, fillG, fillB, fillA float64
-				)
-
-				if running && remain > 0 {
-					ratio := remain.Seconds() / float64(totalSec)
-					angle = -math.Pi/2 + 2*math.Pi*progress*ratio
-					lineColor = err
-					fillR, fillG, fillB, fillA = float64(err.Red()), float64(err.Green()), float64(err.Blue()), 0.3
-				} else {
-					angle = end
-					lineColor = accent
-					fillR, fillG, fillB, fillA = 0.6, 0.6, 0.6, 0.2
-				}
-
-				cr.SetSourceRGBA(fillR, fillG, fillB, fillA)
-				cr.MoveTo(cx, cy)
-				cr.Arc(cx, cy, r, -math.Pi/2, angle)
-				cr.LineTo(cx, cy)
-				cr.Fill()
-
-				cr.SetSourceRGB(float64(lineColor.Red()), float64(lineColor.Green()), float64(lineColor.Blue()))
-				cr.SetLineWidth(10)
-				cr.SetLineCap(cairo.LineCapRound)
-				cr.Arc(cx, cy, r, -math.Pi/2, angle)
-				cr.Stroke()
-
-				handleAngle = angle
-				handleColor = lineColor
-
-				dx := cx + r*math.Cos(handleAngle)
-				dy := cy + r*math.Sin(handleAngle)
-				cr.SetSourceRGB(float64(handleColor.Red()), float64(handleColor.Green()), float64(handleColor.Blue()))
-				cr.Save()
-				cr.Translate(dx, dy)
-				cr.Arc(0, 0, 8, 0, 2*math.Pi)
-				cr.Fill()
-				cr.Restore()
-			}
-		})
-
-		drag := gtk.NewGestureDrag()
-		drag.ConnectDragBegin(func(x, y float64) {
-			dragging = true
-			handleDialing(x, y)
-		})
-		drag.ConnectDragUpdate(func(dx, dy float64) {
-			if dragging {
-				x, y, _ := drag.StartPoint()
-				handleDialing(x+dx, y+dy)
-			}
-		})
-		drag.ConnectDragEnd(func(dx, dy float64) {
-			dragging = false
-
-			if paused {
-				paused = false
-				resumeTimer()
-			} else if !running && totalSec > 0 {
-				startTimer()
-			}
-		})
-
-		click := gtk.NewGestureClick()
-		click.ConnectPressed(func(_ int, x, y float64) {
-			handleDialing(x, y)
-		})
-
-		dial.AddController(drag)
-		dial.AddController(click)
-
-		toggleTimerAction := gio.NewSimpleAction("toggleTimer", nil)
-		toggleTimerAction.ConnectActivate(func(parameter *glib.Variant) {
-			if running {
-				stopTimer()
-			} else if totalSec > 0 {
-				startTimer()
-			}
-		})
-		a.AddAction(toggleTimerAction)
-
-		addTimeAction := gio.NewSimpleAction("addTime", nil)
-		addTimeAction.ConnectActivate(func(parameter *glib.Variant) {
-			if totalSec < 3600 {
-				totalSec += 30
-				if running {
-					remain = time.Duration(totalSec) * time.Second
-				}
-
-				updateDial()
-				updateButtons()
-			}
-		})
-		a.AddAction(addTimeAction)
-
-		removeTimeAction := gio.NewSimpleAction("removeTime", nil)
-		removeTimeAction.ConnectActivate(func(parameter *glib.Variant) {
-			if totalSec > 30 {
-				totalSec -= 30
-				if running {
-					remain = time.Duration(totalSec) * time.Second
-				}
-
-				updateDial()
-				updateButtons()
-			}
-		})
-		a.AddAction(removeTimeAction)
+		var win adw.ApplicationWindow
+		b.GetObject("main_window").Cast(&win)
+		w = &win
 
 		openAboutAction := gio.NewSimpleAction("openAbout", nil)
-		openAboutAction.ConnectActivate(func(parameter *glib.Variant) {
-			aboutDialog.Present(&w.Window)
-		})
+		cb := func(gio.SimpleAction, uintptr) {
+			aboutDialog.Present(&w.Widget)
+		}
+		openAboutAction.ConnectActivate(&cb)
 		a.AddAction(openAboutAction)
-
-		stopAlarmPlaybackAction := gio.NewSimpleAction("stopAlarmPlayback", nil)
-		stopAlarmPlaybackAction.ConnectActivate(func(parameter *glib.Variant) {
-			stopAlarmPlayback()
-
-			a.Activate()
-		})
-		a.AddAction(stopAlarmPlaybackAction)
-
-		updateButtons()
-		updateDial()
 
 		a.AddWindow(&w.Window)
 		w.Present()
-	})
+	}
+	a.ConnectActivate(&cb)
 
-	if code := a.Run(os.Args); code > 0 {
+	if code := a.Run(len(os.Args), os.Args); code > 0 {
 		os.Exit(code)
 	}
 }
