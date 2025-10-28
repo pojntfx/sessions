707df1b4bbebce65a11eec007a649be1ec245c2a
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-10-27 21:45:31 -0700

    docs: Add basic docs to i18n library
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- internal/i18n/gettext.go
+++ internal/i18n/gettext.go
@@ -1,3 +1,5 @@
+// Inspired by https://github.com/diamondburned/gotk4/blob/d44ab4b5b24e200c90c6a9ffb6632ada7e166a79/pkg/core/glib/glib.go#L50-L72
+
 package i18n
 
 import (
@@ -13,6 +15,12 @@ var (
 	gettext               func(msgid string) string
 )
 
+// InitI18n initializes the i18n subsystem. It runs the following C code:
+//
+//	setlocale(LC_ALL, "");
+//	bindtextdomain(domain, dir);
+//	bind_textdomain_codeset(domain, "UTF-8");
+//	textdomain(domain);
 func InitI18n(domain, dir string) error {
 	gettextLibNames, err := getGettextLibraryNames()
 	if err != nil {
@@ -49,6 +57,7 @@ func InitI18n(domain, dir string) error {
 	return nil
 }
 
+// Local localizes a string using gettext.
 func Local(input string) string {
 	return gettext(input)
 }
