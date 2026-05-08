54ad63440170d113a58f54384b652defed8d3470
Felicitas Pojtinger <felicitas@pojtinger.com>
2026-05-07 19:02:06 -0700

    chore: Prepare new release
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- assets/resources/index.go
+++ assets/resources/index.go
@@ -12,7 +12,7 @@ import (
 
 const (
 	AppID      = "com.pojtinger.felicitas.Sessions"
-	AppVersion = "0.1.17"
+	AppVersion = "0.1.18"
 )
 
 //go:generate sh -c "blueprint-compiler batch-compile . . *.blp && glib-compile-resources *.gresource.xml"
